b4c7d0947171646c5f8ad4eed42c65591ca0d4f6
Tero Heikka <tero.heikka@gmail.com>
2025-06-25 14:52:56 +0300

    Osa3 3.15-3.18 final

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const Person = require('./models/person')
 const app = express()
 const morgan = require('morgan')
+const note = require('../../../fullstackopenSampleApp/backend/models/note')
 morgan.token('body', (req) => JSON.stringify(req.body))
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 let persons = []
 
@@ -28,30 +29,39 @@ app.get('/api/persons', morgan('tiny'), (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', morgan('tiny'), (request, response) => {
-  Person.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/persons/:id', morgan('tiny'), (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person){
+        response.json(person)
+      }else{
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', morgan('tiny'), (request, response) => {
-  Person.deleteOne(Person.findById(request.params.id)).then(person => {
-    response.json(person)
+app.delete('/api/persons/:id', morgan('tiny'), (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+  .then(person => {
+    response.status(204).end()
   })
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :body'), (request, response) => {
+app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :body'), (request, response, next) => {
   const body = request.body
   
   if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
-  }else if (persons.length > 0 && persons.map(person => person.name.toLowerCase()).includes(body.name.toLowerCase())){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    })
   }
+  // }else if (persons.map(person => person.name.toLowerCase()).includes(body.name.toLowerCase())){
+  //   return response.status(400).json({ 
+  //     error: 'name must be unique' 
+  //   })
+  // }
 
   Person.findOne(({ name: body.name })).then(existingPerson => {
     if (existingPerson) {
@@ -63,11 +73,60 @@ app.post('/api/persons', morgan(':method :url :status :res[content-length] - :re
       number: body.number
     })
 
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
+    person.save()
+      .then(savedPerson => {
+        response.json(savedPerson)
+      })
+      .catch(error => next(error))
+
   })
 })
+
+app.put('/api/persons/:id', morgan(':method :url :status :res[content-length] - :response-time ms :body'), (request, response, next) => {
+  
+  if (!request.body.name || !request.body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const {name, number} = request.body
+
+  Person.findByIdAndUpdate(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save()
+        .then((udpatedPerson) => {
+          response.json(udpatedPerson)
+        })
+        .catch(error => next(error))
+    })
+    
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
