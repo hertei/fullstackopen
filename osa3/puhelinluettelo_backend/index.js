a37ba3407abf4ae3095807dbb999d4c800cbf3d3
Tero Heikka <tero.heikka@gmail.com>
2025-06-25 12:11:32 +0300

    Osa3 3.13-3.14 final

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const Person = require('./models/person')
 const app = express()
 const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
@@ -6,78 +8,37 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
 app.use(express.static('dist'))
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: "1"
-  },
-  {
-    name: "Ada Lovelace",
-    number: "3298493274",
-    id: "2"
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: "3"
-  },
-  
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: "4"
-  }
-]
+let persons = []
 
 app.get('/', morgan('tiny'), (request, response) => {
   response.send('<h1>Puhelinluettelo Backend</h1>')
 })
 
 app.get('/info', morgan('tiny'), (request, response) => {
-  let numbers = persons.length
-  response.send(`
-    <div>Phonebook has info for ${numbers} people</div>
-    <div>${new Date().toString()}</div>`)
+  Person.countDocuments({}).then(numbers => {
+    response.send(`
+      <div>Phonebook has info for ${numbers} people</div>
+      <div>${new Date().toString()}</div>`)
+  })
 })
 
 app.get('/api/persons', morgan('tiny'), (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', morgan('tiny'), (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/persons/:id', morgan('tiny'), (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.deleteOne(Person.findById(request.params.id)).then(person => {
+    response.json(person)
+  })
 })
-
-const generateId = () => {
-  let newId = (Math.random() * 1000).toFixed(0)
-  let foundUnique = false
-  
-  while (!foundUnique){
-    if (persons.map(n => Number(n.id)).includes(Number(newId))){
-      newId = (Math.random() * 1000).toFixed(0)
-    }else{
-      foundUnique = true
-    }
-
-  }
-  return String(newId)
-}
 
 app.post('/api/persons', morgan(':method :url :status :res[content-length] - :response-time ms :body'), (request, response) => {
   const body = request.body
@@ -86,24 +47,29 @@ app.post('/api/persons', morgan(':method :url :status :res[content-length] - :re
     return response.status(400).json({ 
       error: 'content missing' 
     })
-  }else if (persons.map(person => person.name.toLowerCase()).includes(body.name.toLowerCase())){
+  }else if (persons.length > 0 && persons.map(person => person.name.toLowerCase()).includes(body.name.toLowerCase())){
     return response.status(400).json({ 
       error: 'name must be unique' 
     })
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: generateId()
-  }
+  Person.findOne(({ name: body.name })).then(existingPerson => {
+    if (existingPerson) {
+      return response.status(400).json({ error: `Nimi ${body.name} on jo luettelossa` })
+    }
 
-  persons = persons.concat(person)
+    const person = new Person({
+      name: body.name,
+      number: body.number
+    })
 
-  response.json(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
