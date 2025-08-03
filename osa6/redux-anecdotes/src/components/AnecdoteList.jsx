a014875d423e76b5dc154965a3573787d7a943ec
Tero Heikka <tero.heikka@gmail.com>
2025-08-03 18:51:53 +0300

    osa6 6.3-6.8 anekdootit

---
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addVote } from '../reducers/anecdoteReducer' 
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return (
+    <div>
+      {anecdote.content}
+      <div>
+        has {anecdote.votes} <button onClick={handleClick}>vote</button>
+      </div> 
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return (
+    <div>
+      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => dispatch(addVote(anecdote.id))}
+        />
+      )}
+    </div>
+    )
+}
+
+export default AnecdoteList
+
+/*
+{anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => voteAnecdote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )}
+*/
